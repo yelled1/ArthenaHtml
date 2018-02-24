ac1678d0c47efc6ffa5a15c77526cc3c8fb3e266
S H Yoon <mailhyoon@gmail.com>
2018-02-23 20:13:42 -0500

    4th job

--- Html2JsonCnv.py
+++ Html2JsonCnv.py
@@ -16,16 +16,29 @@ def getCurrencies(fnm="./currencies.txt"):
     with open("currencies.txt", 'r') as fc:
         return [c.split('\t')[0] for c in fc.read().split('\n')]
 
-def parseFile(f):
-    artist, title, price = None, None, None
+def parseFile(f, dbg=True):
+    artist, title, currncy, amount, price = None, None, None, None, None
     with open(f) as fp:
         lns = fp.read()
         soup = BeautifulSoup(lns, "html.parser")
         div = [tg.text for tg in soup.findAll('div')]
         price  = [ d for d in div if re.match(reCURRSTR, d) != None][0]
+        if dbg: print(price)
+        currncy = price[:3]
+        amount  = price[3:].strip()
     artist, title = title_crunch(soup.title.text.strip())
-    if not artist: raise ValueError('artist was not found by the title_crunch function!')
-    return artist, title, price
+    try:
+        assert artist
+        assert title
+        assert currncy
+        assert price
+        assert amount
+    except:
+        raise ValueError('function parseFile returned None value')
+        print(f"prob w/ {f} ; {artist, title, price}")
+        print(div)
+    else:
+        return artist, title, currncy, amount
 
 if __name__ == '__main__':
     #fileDict = {}
@@ -34,15 +47,12 @@ if __name__ == '__main__':
     jsonList = []
     htmlFiles = [f for dir in glob.glob('data/*') for f in glob.glob(dir+'/*')]
     for fnm in htmlFiles:
-        artist_name, title_work, prc = (parseFile(fnm))
+        artist_name, title_work, curr, amt = (parseFile(fnm))
         artists = [x['artist'] for x in jsonList]
+        addDict = {'title': title_work, 'currency': curr, 'amount':amt}
         if artist_name not in artists:
-            jsonList.append({ 'artist': artist_name, 'works': [{'title': title_work, 'price': prc}] })
+            jsonList.append({ 'artist': artist_name, 'works': [addDict] })
         else:
-            jsonList[artists.index(artist_name)]['works'].append({'title': title_work, 'price': prc})
+            jsonList[artists.index(artist_name)]['works'].append( addDict )
     for j in jsonList: print(j)
-
-
-[x['artist'] for x in jsonList]
-
-jsonList
+#[x['artist'] for x in jsonList]
